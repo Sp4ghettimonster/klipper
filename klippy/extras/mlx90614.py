c86512e260a8732e9ea6578e53d0943039c1e7e4
Sp4ghettimonster <114014268+Sp4ghettimonster@users.noreply.github.com>
2022-10-03 19:09:57 +0200

    1.1

--- klippy/extras/mlx90614.py
+++ klippy/extras/mlx90614.py
@@ -1,38 +1,98 @@
-#import logging
+#support for I2C MLX90614 temperature sensor
+#
+#Copyright (C) 2022 Alexander Romboy
+#adapted from LM75 code by Boleslaw Ciesielski
+
+import logging
 from . import bus
 
 MLX90614_CHIP_ADDR = 0x48
 MLX90614_I2C_SPEED = 100000
 MLX90614_REGS = {
-    'TEMP'   : 0x07,    
+    'TEMP'   : 0x07, 
+    'MLX90614_ID1' : 0x3C
 }
+MLX90614_REPORT_TIME = .8
+# Temperature can be sampled at any time but the read aborts
+# the current conversion. Conversion time is 300ms so make
+# sure not to read too often.
+MLX90614_MIN_REPORT_TIME = .5
 
 # define a new temperature sensor
 class MLX90614:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.name = config.get_name().split()[-1]
+        self.reactor = self.printer.get_reactor()
         self.i2c = bus.MCU_I2C_from_config(config, MLX90614_CHIP_ADDR, MLX90614_I2C_SPEED)
-        self.regs = MLX90614_REGS
+        self.mcu = self.i2c.get_mcu()
+        self.report_time = config.getfloat('mlx90614_report_time', MLX90614_REPORT_TIME,
+                                           minval=MLX90614_MIN_REPORT_TIME)
         self.temp = 0
-        self.gcode = self.printer.lookup_object('gcode')
-        self.gcode.register_mux_command("MLX90614", "TEMP", self.name,
-                                        self.cmd_MLX90614_TEMP,
-                                        desc=self.cmd_MLX90614_TEMP_help)
-    cmd_MLX90614_TEMP_help = "Get temperature from MLX90614"
-    def cmd_MLX90614_TEMP(self, params):
-        self.gcode.reset_last_position()
-        self.update_temp()
-        self.gcode.respond_info("MLX90614_TEMP: %s" % (self.temp))
-    def update_temp(self):
-        self.i2c.write([self.regs['TEMP']])
-        data = self.i2c.read(2)
-        self.temp = data[1] + (data[0] << 8)
-        self.temp = self.temp * 0.02 - 273.15
-    def move_update(self, print_time, move):
-        self.update_temp()
+        self.min_temp = 0
+        self.max_temp = 380
+        self.sample_timer = self.reactor.register_timer(self._sample_mlx90614)
+        self.printer.add_object("mlx90614 %s" % (self.name), self)
+        self.printer.register_event_handler("klippy:connect", self.handle_connect)
+   
+    def handle_connect(self):
+        self._init_mlx90614()
+        self.reactor.update_timer(self.sample_timer, self.reactor.NOW)
+
+    def setup_minmax(self, min_temp, max_temp):
+        self.min_temp = min_temp
+        self.max_temp = max_temp
+    
+    def kelvin_to_celsius(self, x):
+        return x[1] + (x[0] << 8) * 0.02 - 273.15
+
+    def _init_mlx90614(self):
+        try:
+            prodid = self.read_register('MLX90614_ID1', 1)[0]
+            logging.info("MLX90614: PRODID: %s" % (prodid))
+        except:
+            pass
+
+    def _sample_mlx90614(self, eventtime):
+        try:
+            sample = self.read_register('TEMP', 2)
+            self.temp = self.kelvin_to_celsius(sample)
+        except Exception:
+            logging.exception("lm75: Error reading data")
+            self.temp = 0.0
+            return self.reactor.NEVER
+
+        if self.temp < self.min_temp or self.temp > self.max_temp:
+            self.printer.invoke_shutdown(
+                "MLX90614 temperature %0.1f outside range of %0.1f:%.01f"
+                % (self.temp, self.min_temp, self.max_temp))
+        
+        measured_time = self.reactor.monotonic()
+        self._callback(self.mcu.estimated_print_time(measured_time), self.temp)
+        return measured_time + self.report_time
+    
+    def setup_callback(self, cb): #passt
+        self._callback = cb
+    
+    def get_report_time_delta(self):
+        return self.report_time
+
+    def read_register(self, reg_name, read_len):
+        # read a single register
+        regs = [MLX90614_REGS[reg_name]]
+        params = self.i2c.i2c_read(regs, read_len)
+        return bytearray(params['response'])
+
+    def write_register(self, reg_name, data):
+        if type(data) is not list:
+            data = [data]
+        reg = MLX90614_REGS[reg_name]
+        data.insert(0, reg)
+        self.i2c.i2c_write(data)
+    
     def get_status(self, eventtime):
-        return {'Temperature': round(self.temp, 2)}
+        return {'Temperature': round(self.temp, 2)} #passt
 
 def load_config(config):
-    return MLX90614(config)
+    pheaters = config.get_printer().load_object(config, "heaters")
+    pheaters.add_sensor_factory("MLX90614", MLX90614)
