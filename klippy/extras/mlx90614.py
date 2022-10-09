da99903edf08586fe7ce85fc3bb295f70b65cefc
Sp4ghettimonster <114014268+Sp4ghettimonster@users.noreply.github.com>
2022-10-09 19:41:26 +0200

    1.1.12.5

--- klippy/extras/mlx90614.py
+++ klippy/extras/mlx90614.py
@@ -67,7 +67,7 @@ class MLX90614:
 
     def _sample_mlx90614(self, eventtime):
         try:
-            sample = self.write_read_register('TEMP', [self.temp], 2)
+            sample = self.write_read_register('TEMP', self.temp, 2)
             self.temp = self.kelvin_to_celsius(sample)
         except Exception:
             logging.exception("MLX90614: Error reading data")
