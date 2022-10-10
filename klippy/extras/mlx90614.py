2d207afae2dfb32ccfef7b9d19cc1ffc9e5eeb26
Sp4ghettimonster <114014268+Sp4ghettimonster@users.noreply.github.com>
2022-10-10 12:57:38 +0200

    1.1.13

--- klippy/extras/mlx90614.py
+++ klippy/extras/mlx90614.py
@@ -12,11 +12,11 @@ MLX90614_REGS = {
     'TEMP'   : 0x07, 
     'MLX90614_ID1' : 0x3C
 }
-MLX90614_REPORT_TIME = 1
+MLX90614_REPORT_TIME = 0.7
 # Temperature can be sampled at any time but the read aborts
 # the current conversion. Conversion time is 300ms so make
 # sure not to read too often.
-MLX90614_MIN_REPORT_TIME = 1
+MLX90614_MIN_REPORT_TIME = 0.5
 
 # define a new temperature sensor
 class MLX90614:
