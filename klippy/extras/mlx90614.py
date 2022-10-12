8f8b53f8c38e23f256ac595a2ed370750111a908
Sp4ghettimonster <114014268+Sp4ghettimonster@users.noreply.github.com>
2022-10-12 16:39:22 +0200

    1.1.16.10

--- klippy/extras/mlx90614.py
+++ klippy/extras/mlx90614.py
@@ -52,22 +52,22 @@ class MLX90614:
     def kelvin_to_celsius(self, x):
         return (x[1] << 8 | x[0]) * 0.02 - 273.15
 
-    def read_register(self, reg_name, read_len):
+    def read_register(self, reg_name):
         # read a single register
-        regs = MLX90614_REGS[reg_name]
-        params = self.i2c.i2c_read(regs, read_len)
+        regs = [MLX90614_REGS[reg_name]]
+        params = self.i2c.i2c_read(regs)
         return bytearray(params['response'])
 
     def _init_mlx90614(self):
         try:
-            prodid = self.read_register('MLX90614_ID1', 1)[0]
+            prodid = self.read_register('MLX90614_ID1')[0]
             logging.info("MLX90614: PRODID %s" % (prodid))
         except:
             pass
 
     def _sample_mlx90614(self, eventtime):
         try:
-            sample = self.read_register('TEMP', 2)
+            sample = self.read_register('TEMP')
             self.temp = self.kelvin_to_celsius(sample)
         except Exception:
             logging.exception("MLX90614: Error reading data")
